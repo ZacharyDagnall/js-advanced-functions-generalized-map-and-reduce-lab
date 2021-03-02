50016cd59cc91aaccb5ba812574659721cf2af38
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-02 14:19:26 -0500

    commit tree 521b6df9c78f3fccb5ec093d8773dcbf79cdb370

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(arr, func) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr[i] = func(arr[i])
+    }
+    return newArr
+}
+
+function reduce(arr, func, start = null) {
+    if (!start) {
+        start = arr[0]
+    } else {
+        start = func(start, arr[0])
+    }
+    let result = start;
+    for (let i = 1; i < arr.length; i++) {
+        result = func(result, arr[i]);
+    }
+    return result;
+}
